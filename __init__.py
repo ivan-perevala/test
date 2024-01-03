285a190a72d02652b95cd5ed0ef9f76111ab18b6
Ivan Perevala <ivan95perevala@gmail.com>
2024-01-03 13:10:54 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,11 @@
+bl_info = {
+    "name": "Camera Projection Painter",
+    "author": "Vlad Erium (ssh4), Ivan Perevala (ivpe)",
+    "version": (4, 0, 3),
+    "blender": (4, 0, 0),
+    "description": "Photogrammetry textures refinement",
+    "location": "Tool Settings > Camera Painter",
+    "support": 'COMMUNITY',
+    "category": "Paint",
+    "doc_url": 'https://github.com/BlenderHQ/camera_projection_painter',
+}
