d7cc7771cd76e5d61d2111da62f35b31bef41b20
Ivan Perevala <ivan95perevala@gmail.com>
2024-01-26 19:20:35 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 bl_info = {
     "name": "Camera Projection Painter 4.1.0",
     "author": "Vlad Erium (ssh4), Ivan Perevala (ivpe)",
-    "version": (4, 1, 1),
-    "blender": (4, 1, 1),
+    "version": (4, 1, 0),
+    "blender": (4, 1, 0),
     "description": "Photogrammetry textures refinement",
     "location": "Tool Settings > Camera Painter",
     "support": 'COMMUNITY',
